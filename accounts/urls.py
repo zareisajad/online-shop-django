5023a31fbe38d446365462bfff41f1e6972b83c0
sajad <zareisajad@protonmail.com>
2022-01-09 13:00:56 +0330

    created reset password

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,5 @@
-from django.urls import path
+from django.urls import path, reverse_lazy
+from django.contrib.auth import views as auth_views
 
 from accounts import views
 
@@ -10,4 +11,35 @@ urlpatterns = [
     path('login/manager/', views.manager_login, name='manager_login'),
     path('logout/', views.user_logout, name='user_logout'),
     path('profile/edit', views.edit_profile, name='edit_profile'),
+    path(
+        'password-reset/',
+        auth_views.PasswordResetView.as_view(
+            template_name='password_reset.html',
+            success_url=reverse_lazy('accounts:password_reset_done'),
+            email_template_name='email_template.html'
+        ),
+        name='password_reset'
+    ),
+    path(
+        'password-reset/done',
+        auth_views.PasswordResetDoneView.as_view(
+            template_name='password_reset_done.html',
+        ),
+        name='password_reset_done'
+    ),
+    path(
+        'password-reset-confirm/<uidb64>/<token>/',
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name='password_reset_confirm.html',
+            success_url=reverse_lazy('accounts:password_reset_complete'),
+        ),
+        name='password_reset_confirm'
+    ),
+    path(
+        'password-reset-complete/',
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name='password_reset_complete.html',
+        ),
+        name='password_reset_complete'
+    ),
 ]
--- online_shop/settings.py
+++ online_shop/settings.py
@@ -139,3 +139,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_URL = 'accounts:user_login'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_SSL = False
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'username@example.com'
+EMAIL_HOST_PASSWORD = 'your-password'
